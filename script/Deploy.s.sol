b94987615b81ea24ff3bad3ea22c20f7a8c78130
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-02-16 14:16:04 +0530

    refactor: Update DeployScript to improve chain ID handling and streamline broadcasting

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,17 +6,13 @@ import "../src/ParityWallet.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract DeployScript is Script {
-    function run() external {
-        // Check chain ID first
-        uint256 chainId;
-        assembly {
-            chainId := chainid()
-        }
+    function setUp() public {}
 
-        if (chainId == 31337) {
-            // Local Anvil chain
-            // Use default Anvil private key
-            vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+    function run() external {
+        // Local Anvil deployment
+        if (block.chainid == 31337) {
+            console.log("Deploying to local Anvil chain (chainId: 31337)");
+            vm.broadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
 
             // Deploy mock token first
             MockParityToken mockToken = new MockParityToken();
@@ -26,16 +22,15 @@ contract DeployScript is Script {
             ParityWallet wallet = new ParityWallet(address(mockToken));
             console.log("ParityWallet deployed to:", address(wallet));
         } else {
-            // Network deployment - require environment variables
+            // Network deployment
+            console.log("Deploying to network (chainId: %s)", block.chainid);
             uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
             address tokenAddress = vm.envAddress("TOKEN_ADDRESS");
 
-            vm.startBroadcast(deployerPrivateKey);
+            vm.broadcast(deployerPrivateKey);
             ParityWallet wallet = new ParityWallet(tokenAddress);
             console.log("ParityWallet deployed to:", address(wallet));
         }
-
-        vm.stopBroadcast();
     }
 }
 
